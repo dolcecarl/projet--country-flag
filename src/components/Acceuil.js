eda1d2a60f7759cde50d658e3b89bef694c77f1d
Dolce Carl <59517957+dolcecarl@users.noreply.github.com>
2021-10-18 20:19:32 -0400

    Add files via upload

---
+++ src/components/Acceuil.js
@@ -0,0 +1,34 @@
+
+import './Acceuil.css';
+
+const Acceuil = () =>{
+
+    
+    return(
+             <div className="margin-background">
+
+                <div className="acceuil-background"
+
+                style={{backgroundImage: "url(" +  '/imageBack.png' + ")"}}>
+
+                </div>
+
+                 
+             </div>
+       
+        
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export default Acceuil;
